aff439c73a1220c4d320f8cf39f5239d41a02891
Prince Shukla <shuklavg800@gmail.com>
2026-02-17 19:33:39 +0530

    Time: 0 ms (100%), Space: 22.2 MB (40.43%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n = asteroids.size();
         stack<int> st;
-        vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            // if left encountered and the stack is empty or the top is left
-            // going
-            if ((st.empty() || st.top() < 0) && asteroids[i] < 0) {
-                st.push(asteroids[i]);
-            }
-            // if right encountered push it
-            else if (asteroids[i] > 0) {
-                st.push(asteroids[i]);
-            } else {
-                // left encountered and the stack is not empty
 
-                // case 1 when right going one has higher impact
-                // donot push the left going to the stack
-                if (st.top() > abs(asteroids[i])) {
-                    continue;
-                } else {
-                    // case 3 when left going one has higher impact
-                    // keep popping untill a right going one with higher impact
-                    // encountered or left going encountered
+        for (int a : asteroids) {
 
-                    while (!st.empty() &&
-                           (st.top() < abs(asteroids[i]) && st.top() > 0)) {
-                        st.pop();
-                    }
-                    // case 2 :equal impact case when left magnitude equal
-                    // to the right
-                    if (!st.empty() && st.top() == abs(asteroids[i])) {
-                        st.pop();
-                        continue;
-                    }
-                    if ((st.empty() || st.top() < 0) && asteroids[i] < 0) {
-                        st.push(asteroids[i]);
-                    }
+            bool destroyed = false;
+
+            while (!st.empty() && st.top() > 0 && a < 0) {
+
+                if (st.top() < -a) {
+                    st.pop();          // right asteroid explodes
+                }
+                else if (st.top() == -a) {
+                    st.pop();          // both explode
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    destroyed = true;  // left asteroid explodes
+                    break;
                 }
             }
+
+            if (!destroyed) {
+                st.push(a);
+            }
         }
-        while (!st.empty()) {
-            ans.push_back(st.top());
+
+        vector<int> ans(st.size());
+        for (int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
             st.pop();
         }
-        reverse(ans.begin(), ans.end());
+
         return ans;
     }
 };
