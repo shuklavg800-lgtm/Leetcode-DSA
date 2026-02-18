732537b9bf853c275d2eea8c6932949d05b85eec
Prince Shukla <shuklavg800@gmail.com>
2026-02-18 20:37:50 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.78%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -6,22 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    void helper(TreeNode* root, vector<int>& ans){
-        if(root==nullptr){
-            return;
-        }
-        ans.push_back(root->val);
-        helper(root->left,ans);
-        helper(root->right,ans);
-    }
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+        // using stack
         vector<int> ans;
-        helper(root,ans);
+        stack<TreeNode*> st;
+        if (!root) {
+            return ans;
+        }
+        st.push(root);
+        while (!st.empty()) {
+            // if(!st.empty){
+                TreeNode* node=st.top();
+                st.pop();
+                ans.push_back(node->val);
+            // }
+            if(node->right)
+                st.push(node->right);
+            // left pushed after right to simulate the behaviour of root left then right
+            // right bcz stack follows LIFO
+            if(node->left)
+                st.push(node->left);
+        }
         return ans;
     }
 };
